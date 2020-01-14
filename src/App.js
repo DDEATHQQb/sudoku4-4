e8efad71ec0ed115eb2f1dca49db4898d2f1317e
DDEATHQQb <->
2020-01-14 15:57:33 +0700

    dir3

--- src/App.js
+++ src/App.js
@@ -1,71 +1,8 @@
 import React, { Component } from "react";
 
-// import Cell from "./Cell";
 import "./App.css";
-class Cell extends Component {
-  render() {
-    return (
-      <div
-        className={`cell ${this.props.isInitial ? "initial" : ""}`}
-        onClick={e => {
-          if (this.props.isInitial) {
-            return;
-          }
-          this.props.onChange((this.props.number + 1) % 5);
-        }}
-      >
-        {this.props.number !== 0 && this.props.number}
-      </div>
-    );
-  }
-}
-class Board extends Component {
-  state = {
-    board: [
-      [1, 2, 3, 4],
-      [3, 4, 0, 0],
-      [2, 0, 4, 0],
-      [4, 0, 0, 2]
-    ],
-    initial: [
-      [true, true, true, true],
-      [true, true, false, false],
-      [true, false, true, false],
-      [true, false, false, true]
-    ],
-    statusText: ""
-  };
-  submit = () => {
-    // const isValid = validate(this.state.board);
-    this.setState({
-      statusText: true ? "Board is complete!!" : "Board is invalid"
-    });
-  };
-  render() {
-    return (
-      <div>
-        <div className="board">
-          {this.state.board.map((row, i) =>
-            row.map((numb, j) => (
-              <Cell
-                key={`cell-${i}-${j}`}
-                isInitial={this.state.initial[i][j]}
-                number={numb}
-                onChange={newnumber => {
-                  const { board } = this.state;
-                  board[i][j] = newnumber;
-                  this.setState({ board });
-                }}
-              />
-            ))
-          )}
-        </div>
-        <button onClick={this.submit}>Submit</button>
-        <p>{this.statusText}</p>
-      </div>
-    );
-  }
-}
+
+import Board from "./Board";
 
 class App extends Component {
   render() {
--- src/Cell.js
+++ src/Cell.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default ({ isInit, number, onClick }) => (
-  <div
-    onClick={isInit ? null : onClick}
-    className={`cell ${isInit ? "initial" : ""}`}
-  >
-    {!!number && number}
-  </div>
-);
+// class Cell extends Component {
+//   render() {
+//     return (
+//       <div
+//         className={`cell ${this.props.isInitial ? "initial" : ""}`}
+//         onClick={e => {
+//           if (this.props.isInitial) {
+//             return;
+//           }
+//           this.props.onChange((this.props.number + 1) % 5);
+//         }}
+//       >
+//         {this.props.number !== 0 && this.props.number}
+//       </div>
+//     );
+//   }
+// }
+const Cell = ({ isInitial, number, onChange }) => {
+  return (
+    <div
+      className={`cell ${isInitial ? "initial" : ""}`}
+      onClick={e => {
+        if (isInitial) {
+          return;
+        }
+        onChange((number + 1) % 5);
+      }}
+    >
+      {number !== 0 && number}
+    </div>
+  );
+};
+
+export default Cell;
