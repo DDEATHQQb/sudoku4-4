22c4ee6c9dcea34e5360cfe2603270d4833d1b6e
DDEATHQQb <->
2020-01-14 14:52:29 +0700

    sub2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
 
-import Cell from "./Cell";
+// import Cell from "./Cell";
 import "./App.css";
-
-class App extends Component {
+class Cell extends Component {
+  render() {
+    return (
+      <div
+        className={`cell ${this.props.isInitial ? "initial" : ""}`}
+        onClick={e => {
+          if (this.props.isInitial) {
+            return;
+          }
+          this.props.onChange((this.props.number + 1) % 5);
+        }}
+      >
+        {this.props.number !== 0 && this.props.number}
+      </div>
+    );
+  }
+}
+class Board extends Component {
   state = {
     board: [
       [1, 2, 3, 4],
@@ -11,70 +27,51 @@ class App extends Component {
       [2, 0, 4, 0],
       [4, 0, 0, 2]
     ],
-    initFlags: [
+    initial: [
       [true, true, true, true],
       [true, true, false, false],
       [true, false, true, false],
       [true, false, false, true]
     ],
-    complete: false,
-    invalid: false
-  };
-  handleClick = (i, j, number) => {
-    const { board } = this.state;
-    board[i][j] = (board[i][j] + 1) % 5;
-    this.setState({ board });
+    statusText: ""
   };
   submit = () => {
-    const { board } = this.state;
-    let isValid = true;
-    for (let i = 0; i < 4; i++) {
-      const horizontal = new Set();
-      const vertical = new Set();
-      const square = new Set();
-      for (let j = 0; j < 4; j++) {
-        horizontal.add(board[i][j]);
-        vertical.add(board[j][i]);
-        square.add(
-          board[2 * (i % 2) + (j % 2)][
-            2 * Math.floor(i / 2) + Math.floor(j / 2)
-          ]
-        );
-      }
-      horizontal.delete(0);
-      vertical.delete(0);
-      square.delete(0);
-      if (horizontal.size !== 4 || vertical.size !== 4 || square.size !== 4) {
-        isValid = false;
-      }
-    }
+    // const isValid = validate(this.state.board);
     this.setState({
-      invalid: !isValid,
-      complete: isValid
+      statusText: true ? "Board is complete!!" : "Board is invalid"
     });
   };
   render() {
     return (
-      <div className="App">
+      <div>
         <div className="board">
-          {this.state.board.map((row, i) => {
-            row.map((cell, j) => (
+          {this.state.board.map((row, i) =>
+            row.map((numb, j) => (
               <Cell
                 key={`cell-${i}-${j}`}
-                onClick={() => this.handleClick(i, j, cell)}
-                number={cell}
-                isInitial={this.state.initFlags[i][j]}
+                isInitial={this.state.initial[i][j]}
+                number={numb}
+                onChange={newnumber => {
+                  const { board } = this.state;
+                  board[i][j] = newnumber;
+                  this.setState({ board });
+                }}
               />
-            ));
-          })}
+            ))
+          )}
         </div>
         <button onClick={this.submit}>Submit</button>
-        {this.state.complete && (
-          <p style={{ color: "green" }}>Board is complete!</p>
-        )}
-        {this.state.invalid && (
-          <p style={{ color: "red" }}>Board is invalid!</p>
-        )}
+        <p>{this.statusText}</p>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Board />
       </div>
     );
   }
--- src/Cell.js
+++ src/Cell.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 export default ({ isInit, number, onClick }) => (
-  <div>
-    onClick = {isInit ? null : onClick}
-    className = {`cell ${isInit ? "initial" : ""}`}>{!!number && number}
+  <div
+    onClick={isInit ? null : onClick}
+    className={`cell ${isInit ? "initial" : ""}`}
+  >
+    {!!number && number}
   </div>
 );
